c4b3de1f47d60a636732f60a9faeac2ae6a39aa5
Médéric Hurier (Fmind) <github@fmind.dev>
2024-02-23 22:16:24 +0100

    Test CI/CD

--- tasks/containers.py
+++ tasks/containers.py
@@ -21,15 +21,15 @@ def compose(ctx: Context) -> None:
 @task(pre=[packages.build])
 def build(ctx: Context) -> None:
     """Build the container image."""
-    ctx.run(f"docker build -t fmind/{ctx.project.name}:latest .")
+    ctx.run(f"docker build -t ghcr.io/fmind/{ctx.project.name}:latest .")
 
 
 @task
 def run(ctx: Context) -> None:
     """Run the container image."""
-    ctx.run(f"docker run --rm fmind/{ctx.project.name}:latest")
+    ctx.run(f"docker run --rm ghcr.io/fmind/{ctx.project.name}:latest")
 
 
-@task(pre=[build, run], default=True)
+@task(pre=[build], default=True)
 def all(_: Context) -> None:
     """Run all container tasks."""
